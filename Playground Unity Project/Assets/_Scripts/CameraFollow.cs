102e015e2502d8f664fa89cdf40172f7719b1612
skinnie03 <48211881+skinnie03@users.noreply.github.com>
2019-03-27 22:51:57 +0800

    change color script done

---
+++ Playground Unity Project/Assets/_Scripts/CameraFollow.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform playerTransform;
+    [SerializeField] Vector3 camOffset;
+
+    private void Update()
+    {
+        transform.position = playerTransform.position + camOffset;
+    }
+}
