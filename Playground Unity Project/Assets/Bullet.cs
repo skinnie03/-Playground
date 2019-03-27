d1fd837d9b0239173ac2d1f10f58d9daef818318
skinnie03 <48211881+skinnie03@users.noreply.github.com>
2019-03-27 17:59:31 +0800

    Initial commit

---
+++ Playground Unity Project/Assets/Bullet.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float bulletSpeed = 20f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = transform.forward * bulletSpeed;
+    }
+
+}
---
+++ Playground Unity Project/Assets/_Scripts/Shotgun.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shotgun : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform firepoint;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Shotgun shooting");
+            ShootMultipleProjectiles();
+        }
+    }
+
+
+
+
+    public void ShootMultipleProjectiles()
+    {
+        Vector3 rotVector = firepoint.rotation.eulerAngles;
+        Debug.Log(rotVector);
+        
+        Instantiate(bullet, firepoint.position, firepoint.rotation);
+
+        rotVector.y += 15f;
+        Debug.Log(rotVector);
+        Instantiate(bullet, firepoint.position, Quaternion.Euler(rotVector));
+
+        rotVector.y -= 30f;
+        Debug.Log(rotVector);
+        Instantiate(bullet, firepoint.position, Quaternion.Euler(rotVector));
+    }
+}
