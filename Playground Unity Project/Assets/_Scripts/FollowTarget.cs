ea7d7e8d40c26df68bb1e130d0395c4464b2ceee
skinnie03 <48211881+skinnie03@users.noreply.github.com>
2019-03-29 23:19:42 +0800

    hotfix

---
+++ Playground Unity Project/Assets/_Scripts/FollowTarget.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTarget : MonoBehaviour
+{
+    public Transform targetTransform;
+
+    private void Update()
+    {
+        transform.position = targetTransform.position;
+    }
+}
