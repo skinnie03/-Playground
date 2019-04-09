be88bfad7f085c39ac46107bc43f9b9865e8ba3d
skinnie03 <48211881+skinnie03@users.noreply.github.com>
2019-04-09 17:30:31 +0800

    update

---
+++ Playground Unity Project/Assets/Lab/_Scripts/Ability.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Ability", menuName = "Ability")]
+public class Ability : ScriptableObject {
+
+    public new string name;
+    public string description;
+
+    public GameObject prefab;
+    public Sprite artwork;
+
+    public float damage;
+    public float coolDown;
+    public float castTime;
+
+    public string targetingType;
+    public string unitType;
+    public string effectType;
+    public string abilityType;
+    
+
+    public void CastAbility()
+    {
+
+    }
+}
---
+++ Playground Unity Project/Assets/Lab/_Scripts/Player/CastAbility.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CastAbility : MonoBehaviour
+{
+    public Ability ability;
+    public Transform firepoint;
+
+    private float coolDownTimer;
+    private float castTimeTimer;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            PerformCastAbility();
+        }
+
+        if (coolDownTimer > 0) { coolDownTimer -= Time.deltaTime; }
+        if (castTimeTimer > 0) { castTimeTimer -= Time.deltaTime; }
+    }
+
+    public void PerformCastAbility()
+    {
+        if (coolDownTimer <= 0)
+        {
+            coolDownTimer = ability.coolDown;
+            castTimeTimer = ability.castTime;
+            Instantiate(ability.prefab, firepoint.position, firepoint.rotation);
+        }
+    }
+}
